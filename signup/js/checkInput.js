76610a570e824a68d118131c56624b97a6797d82
hong <daggax8235@gmail.com>
2024-03-07 23:53:07 +0900

    checkInput 중복되는 부분 개선

--- signup/js/checkInput.js
+++ signup/js/checkInput.js
@@ -2,23 +2,36 @@ import { InputType } from '/type.js'
 
 const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
 
+function setError(input, error, errorMessage) {
+  error.textContent = errorMessage
+  input.style.border = '2px solid red'
+}
+
+function clearError(input, error) {
+  error.textContent = ''
+  input.style.border = '1px var(--gray-400) solid'
+}
+
+function checkEmpty(input, error, errorMessage) {
+  if (input.value.trim() === '') {
+    setError(input, error, errorMessage)
+    return true
+  }
+  return false
+}
+
 export default function checkInput(inputId, errorId, type) {
   const input = document.getElementById(inputId)
   const error = document.getElementById(errorId)
 
   switch (type) {
     case InputType.EMAIL:
-      if (input.value.trim() === '') {
-        error.textContent = '이메일을 입력해 주세요.'
-        input.style.border = '2px solid red'
-        return
-      }
+      if (checkEmpty(input, error, '이메일을 입력해 주세요.')) return
       if (!emailRegex.test(input.value.trim())) {
-        error.textContent = '올바른 이메일 주소가 아닙니다.'
-        input.style.border = '2px solid red'
+        setError(input, error, '올바른 이메일 주소가 아닙니다.')
         return
       }
-      error.textContent = ''
+      clearError(input, error)
       break
 
     case InputType.PASSWORD:
@@ -29,36 +42,27 @@ export default function checkInput(inputId, errorId, type) {
         'signup-passwordConfirmError',
       )
 
-      if (input.value.trim() === '') {
-        error.textContent = '비밀번호를 입력해 주세요.'
-        input.style.border = '2px solid red'
-        return
-      }
+      if (checkEmpty(input, error, '비밀번호를 입력해 주세요.')) return
       if (
         passwordConfirmInput.value.trim() !== '' &&
         input.value.trim() !== passwordConfirmInput.value.trim()
       ) {
-        passwordConfirmError.textContent = '비밀번호가 일치하지 않습니다.'
-        input.style.border = '2px solid red'
+        setError(input, error, '비밀번호가 일치하지 않습니다.')
         return
       }
 
-      passwordConfirmError.textContent = ''
+      clearError(input, error)
+      clearError(passwordConfirmInput, passwordConfirmError)
       break
 
     case InputType.PASSWORD_CONFIRM:
       const passwordInput = document.getElementById('signup-password')
-      if (input.value.trim() === '') {
-        error.textContent = '비밀번호 확인을 입력해 주세요.'
-        input.style.border = '2px solid red'
-        return
-      }
+      if (checkEmpty(input, error, '비밀번호 확인을 입력해 주세요.')) return
       if (input.value.trim() !== passwordInput.value.trim()) {
-        error.textContent = '비밀번호가 일치하지 않습니다.'
-        input.style.border = '2px solid red'
+        setError(input, error, '비밀번호가 일치하지 않습니다.')
         return
       }
-      error.textContent = ''
+      clearError(input, error)
       break
 
     default:
