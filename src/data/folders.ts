f35089a7da64213440bdb0ac5ad76957ce380b84
daggax <daggax8235@gmail.com>
2024-04-27 03:41:38 +0900

    리셋

--- src/data/folders.ts
+++
@@ -1,7 +0,0 @@
-import api from '@/lib/api'
-import { Folder } from '@/model'
-
-export const getFolderData = async (): Promise<Folder> => {
-  const response = await api<{ folder: Folder }>('GET', '/sample/folder')
-  return response.data.folder
-}
