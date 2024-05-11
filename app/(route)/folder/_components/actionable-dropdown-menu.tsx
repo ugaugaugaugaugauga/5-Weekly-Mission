27700aea33663a6c74773cc8716eb766ee56e3cf
daggax <daggax8235@gmail.com>
2024-05-11 19:08:59 +0900

    배포 에러 수정3

--- app/(route)/folder/_components/actionable-dropdown-menu.tsx
+++ app/(route)/folder/_components/actionable-dropdown-menu.tsx
@@ -4,6 +4,10 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
+import { Ellipsis } from 'lucide-react'
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import { useState } from 'react'
 import {
   AlertDialog,
   AlertDialogCancel,
@@ -12,18 +16,16 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
-  AlertDialogTrigger,
 } from '@/components/ui/alert-dialog'
-
-import { Ellipsis } from 'lucide-react'
-import { Input } from '@/components/ui/input'
-import { Button } from '@/components/ui/button'
 
 type Props = {
   title: string
 }
 
 const ActionableDropdownMenu = ({ title }: Props) => {
+  const [showAddFolderDialog, setShowAddFolderDialog] = useState(false)
+  const [showDeleteFolderDialog, setShowDeleteFolderDialog] = useState(false)
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className='outline-none '>
@@ -34,50 +36,65 @@ const ActionableDropdownMenu = ({ title }: Props) => {
           onClick={(event: React.MouseEvent) => event.preventDefault()}
           className='flex justify-center cursor-pointer'
         >
-          <AlertDialog>
-            <AlertDialogTrigger>폴더에 추가</AlertDialogTrigger>
-            <AlertDialogContent>
-              <AlertDialogHeader>
-                <AlertDialogTitle>폴더 추가</AlertDialogTitle>
-                <AlertDialogDescription>
-                  원하는 폴더를 추가해 보세요!
-                </AlertDialogDescription>
-                <Input placeholder='내용 입력' />
-              </AlertDialogHeader>
-              <AlertDialogFooter className='grid grid-cols-2'>
-                <AlertDialogCancel>취소</AlertDialogCancel>
-                <Button variant='primary'>추가하기</Button>
-              </AlertDialogFooter>
-            </AlertDialogContent>
-          </AlertDialog>
+          <button onClick={() => setShowAddFolderDialog(true)}>
+            폴더에 추가
+          </button>
         </DropdownMenuItem>
 
         <DropdownMenuItem
           className='flex justify-center cursor-pointer'
           onClick={(event: React.MouseEvent) => event.preventDefault()}
         >
-          <AlertDialog>
-            <AlertDialogTrigger>삭제하기</AlertDialogTrigger>
-            <AlertDialogContent>
-              <AlertDialogHeader>
-                <AlertDialogTitle>폴더 삭제</AlertDialogTitle>
-                <AlertDialogDescription>
-                  정말로
-                  <span className='font-bold text-indigo-500 mx-1'>
-                    {title}
-                  </span>
-                  폴더를 삭제 하시겠습니까?
-                </AlertDialogDescription>
-                <AlertDialogDescription></AlertDialogDescription>
-              </AlertDialogHeader>
-              <AlertDialogFooter className='grid grid-cols-2'>
-                <AlertDialogCancel>취소</AlertDialogCancel>
-                <Button variant='destructive'>삭제하기</Button>
-              </AlertDialogFooter>
-            </AlertDialogContent>
-          </AlertDialog>
+          <button onClick={() => setShowDeleteFolderDialog(true)}>
+            삭제하기
+          </button>
         </DropdownMenuItem>
       </DropdownMenuContent>
+
+      {/* 추가 폴더 다이얼로그 */}
+      {showAddFolderDialog && (
+        <AlertDialog>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>폴더 추가</AlertDialogTitle>
+              <AlertDialogDescription>
+                원하는 폴더를 추가해 보세요!
+              </AlertDialogDescription>
+              <Input placeholder='내용 입력' />
+            </AlertDialogHeader>
+            <AlertDialogFooter className='grid grid-cols-2'>
+              <AlertDialogCancel onClick={() => setShowAddFolderDialog(false)}>
+                취소
+              </AlertDialogCancel>
+              <Button variant='primary'>추가하기</Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+      )}
+
+      {/* 삭제 폴더 다이얼로그 */}
+      {showDeleteFolderDialog && (
+        <AlertDialog>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>폴더 삭제</AlertDialogTitle>
+              <AlertDialogDescription>
+                정말로
+                <span className='font-bold text-indigo-500 mx-1'>{title}</span>
+                폴더를 삭제 하시겠습니까?
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter className='grid grid-cols-2'>
+              <AlertDialogCancel
+                onClick={() => setShowDeleteFolderDialog(false)}
+              >
+                취소
+              </AlertDialogCancel>
+              <Button variant='destructive'>삭제하기</Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+      )}
     </DropdownMenu>
   )
 }
